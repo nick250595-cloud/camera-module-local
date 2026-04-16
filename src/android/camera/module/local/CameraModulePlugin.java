9bdb04837eee6988bacdf917f39df1f9529c142e
nick250595-cloud <nick.250595@gmail.com>
2026-04-16 16:12:53 -0500

    Update CameraModulePlugin.java

--- src/android/camera/module/local/CameraModulePlugin.java
+++ src/android/camera/module/local/CameraModulePlugin.java
@@ -33,7 +33,7 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
 import com.google.mlkit.vision.common.InputImage;
